f32eb8cb2d1f4e6097df3f6f31def564c348d91f
chenyong <196510921@qq.com>
2015-12-03 14:57:04 +0800

    1.sdk小版本更新，增加智能家居demo 2.sdk目录更改

---
+++ 01.00.00.00.150925/ST_SDK/Projects/Multi/Application/Blink/MDK-ARM/gp_timer.d
@@ -0,0 +1,6 @@
+.\mdk-arm\gp_timer.o: ..\..\..\..\Middlewares\ST\STM32_BlueNRG\SimpleBlueNRG_HCI\utils\gp_timer.c
+.\mdk-arm\gp_timer.o: ..\..\..\..\Middlewares\ST\STM32_BlueNRG\SimpleBlueNRG_HCI\includes\clock.h
+.\mdk-arm\gp_timer.o: ..\..\..\..\Middlewares\ST\STM32_BlueNRG\SimpleBlueNRG_HCI\includes\hal_types.h
+.\mdk-arm\gp_timer.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\mdk-arm\gp_timer.o: ..\..\..\..\Middlewares\ST\STM32_BlueNRG\SimpleBlueNRG_HCI\includes\gp_timer.h
+.\mdk-arm\gp_timer.o: ..\..\..\..\Middlewares\ST\STM32_BlueNRG\SimpleBlueNRG_HCI\includes\ble_status.h
---
+++ 01.00.00.00.150925/ST_SDK/Projects/Multi/Application/Blink/MDK-ARM/list.d
@@ -0,0 +1,4 @@
+.\mdk-arm\list.o: ..\..\..\..\Middlewares\ST\STM32_BlueNRG\SimpleBlueNRG_HCI\utils\list.c
+.\mdk-arm\list.o: ..\..\..\..\Middlewares\ST\STM32_BlueNRG\SimpleBlueNRG_HCI\includes\hal_types.h
+.\mdk-arm\list.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\mdk-arm\list.o: ..\..\..\..\Middlewares\ST\STM32_BlueNRG\SimpleBlueNRG_HCI\includes\list.h
